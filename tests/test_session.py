65b8ec1a1d3f61b621273677975a5c0a338ec0da
Philip Xu <pyx@xrefactor.com>
2017-04-28 22:06:31 -0400

    Passed 100% test coverage

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,3 +1,5 @@
+import pytest
+
 from sanic import Sanic, response
 
 import sanic_cookiesession
@@ -6,6 +8,7 @@ import sanic_cookiesession
 def test_session():
     app = Sanic('test_app')
     app.config['SESSION_COOKIE_SECRET_KEY'] = "u can't c me"
+    # app.config['SESSION_COOKIE_DOMAIN'] = 'http://127.0.0.1:42101'
 
     sanic_cookiesession.init_app(app)
 
@@ -103,3 +106,90 @@ def test_cookies_tempered():
     assert resp.text == 'yes'
     cookies = resp.cookies
     assert cookies
+
+
+def test_session_cookie_domain():
+    app = Sanic('test_app')
+    app.config['SESSION_COOKIE_SECRET_KEY'] = "u can't c me"
+    app.config['SESSION_COOKIE_DOMAIN'] = '.example.com'
+
+    sanic_cookiesession.init_app(app)
+
+    @app.get('/')
+    async def index(request):
+        value = request['session']['counter'] = 0
+        return response.text(value)
+
+    req, resp = app.test_client.get('/')
+    assert resp.status == 200
+    assert resp.text == '0'
+    cookies = resp.cookies
+    assert cookies['_session']['domain'] == '.example.com'
+
+
+def test_not_overwrite_existing_session():
+    app = Sanic('test_app')
+    app.config['SESSION_COOKIE_SECRET_KEY'] = "u can't c me"
+    # set session name so that this test do not rely on default setting
+    app.config['SESSION_NAME'] = 'session'
+
+    this_is_the_session = {}
+
+    # add session object before Sanic-CookieSession
+    @app.middleware('request')
+    async def add_session(request):
+        request['session'] = this_is_the_session
+
+    sanic_cookiesession.init_app(app)
+
+    @app.get('/')
+    async def index(request):
+        assert this_is_the_session is request['session']
+        request['session']['name'] = 'penny'
+        return response.text('')
+
+    # hit
+    req, resp = app.test_client.get('/')
+    assert resp.status == 200
+    assert resp.text == ''
+    assert this_is_the_session['name'] == 'penny'
+
+
+def test_delete_session_in_request_will_create_new_object():
+    app = Sanic('test_app')
+    app.config['SESSION_COOKIE_SECRET_KEY'] = "u can't c me"
+
+    hit_count = 0
+
+    class Session(dict):
+        def __init__(self, *args, **kwargs):
+            nonlocal hit_count
+            hit_count += 1
+            super().__init__(*args, **kwargs)
+
+    sanic_cookiesession.init_app(app, session_type=Session)
+
+    @app.get('/')
+    async def index(request):
+        # we will only hit this once in this test, so it holds true
+        assert hit_count == 1
+        # then delete it, response middleware will create a new one to be
+        # saved in the cookie-based session storage
+        del request['session']
+        return response.text('')
+
+    # hit
+    req, resp = app.test_client.get('/')
+    assert resp.status == 200
+    assert resp.text == ''
+    assert hit_count == 2
+
+
+def test_secret_key_required():
+    app = Sanic('test_app')
+    assert app.config.get('SECRET_KEY') is None
+    assert app.config.get('SESSION_COOKIE_SECRET_KEY') is None
+
+    with pytest.raises(RuntimeError):
+        # no secret key
+        sanic_cookiesession.init_app(app)
